881c5e3019d4e38f6134d21186b68063317aa0d8
AmuthaMohanraj <rajmohanraj2304@gmail.com>
2023-06-01 19:34:35 +0530

    add companyapi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const mysql=require('mysql')
 const port=3000;
 const app=express();
+const s='success'
 
 const cors=require('cors')
 app.use(cors());
@@ -21,7 +22,7 @@ app.get('/company',(req,res)=>{
     if(error){
      console.log("error is",error)
     }else{
-     console.log("the result is",result)
+     console.log("the result is",s)
      res.json(result)
     }
   })
@@ -30,32 +31,32 @@ app.get('/company',(req,res)=>{
 
 // AboutCompanyDetails
 app.get('/getCompanyById/:id',(req,res)=>{
-    connection.query("select*from companydetails where company_id=?",[req.params.id],(error,result)=>{
+    connection.query("select*from companydetails where company_id=? And isActive=?",[req.params.id,1],(error,result)=>{
         if(error){
           console.log("error is",error);
         }else{
-            console.log("The Solution is",result);
+            console.log("The Solution is",s,result);
             res.json(result)
         }
     })
 }
 )
 
-// createUser
+// createCompanyList
 app.post('/createCompanyList',(req,res)=>{
   connection.query("insert into companydetails(company_name, industry, address, city, state_province, country, phone_number, email, website, ceo_owner, year_founded, revenue, employee_count, description) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
   ,[req.body.company_name,req.body.industry,req.body.address,req.body.city,req.body.state_province,req.body.country,req.body.phone_number,req.body.email,req.body.website,req.body.ceo_owner,req.body.year_founded,req.body.revenue,req.body.employee_count,req.body.description],(error,result)=>{
     if(error){
       console.log('error is',error);
     }else{
-      console.log('The result is',result);
+      console.log('The result is',s);
       console.log(res.json());
     }
   })
 })
 
-// UpdateUser
-app.put('/updateUser',(req,res)=>{
+// UpdateCompanyList
+app.put('/updateCompanyList',(req,res)=>{
   connection.query(
     'UPDATE companydetails SET company_name = ?, industry = ?, address = ?, city = ?, state_province = ?, country = ?, phone_number = ?, email = ?, website = ?, ceo_owner = ?, year_founded = ?, revenue = ?, employee_count = ?, description = ? WHERE company_id = ?',
     [req.body.company_name, req.body.industry, req.body.address, req.body.city, req.body.state_province, req.body.country, req.body.phone_number, req.body.email, req.body.website, req.body.ceo_owner, req.body.year_founded, req.body.revenue, req.body.employee_count, req.body.description, req.body.company_id],
@@ -63,7 +64,7 @@ app.put('/updateUser',(req,res)=>{
       if (error) {
         console.log('Error:', error);
       } else {
-        console.log('Result:', result,'update sucussfully');
+        console.log('Result:', s,'update sucussfully');
         console.log(res.json());
       }
     }
@@ -71,8 +72,8 @@ app.put('/updateUser',(req,res)=>{
   
 })
 
-// deletUser
-app.put('/deleteUser',(req,res)=>{
+// deleteCompanyList
+app.put('/deleteCompanyList',(req,res)=>{
   connection.query("update companydetails set isActive=0 WHERE company_id = ?",[req.body.company_id],(error,result)=>{
     if(error){
       console.log('error')
@@ -85,14 +86,15 @@ app.put('/deleteUser',(req,res)=>{
 })
 
 
+
 // EmployeesList
 app.get('/employee',(req,res)=>{
-    connection.query("select*from employeedetails",(error,result)=>{
+    connection.query("select*from employeedetails where isActive=1",(error,result)=>{
       if(error){
        console.log("error is",error)
        return
       }else{
-       console.log("the result is",result)
+       console.log("the result is",s)
        res.json(result)
       }
     })
@@ -100,18 +102,60 @@ app.get('/employee',(req,res)=>{
 
 // aboutEmployeesDetails
   app.get('/getEmployeeById/:id',(req,res)=>{
-    connection.query("select*from employeedetails where employee_id=?",[req.params.id],(error,result)=>{
+    connection.query("select*from employeedetails where employee_id=? And isActive=?",[req.params.id,1],(error,result)=>{
       if(error){
         console.log("The error is",error)
         return
       }else{
-        console.log("The result is",result)
+        console.log("The result is",s)
         res.json(result)
       }
     })
   })
 
+  // createEmployee
+app.post('/createEmployeeList', (req, res) => {
+  connection.query("INSERT INTO employeedetails (first_name, last_name, email, phone_number, hire_date, job_title, department, salary, manager_id, isActive) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [req.body.first_name, req.body.last_name, req.body.email, req.body.phone_number, req.body.hire_date, req.body.job_title, req.body.department, req.body.salary, req.body.manager_id, 1],
+    (error, result) => {
+      if (error) {
+        console.log('error is', error);
+        return;
+      } else {
+        console.log('The solution is', result);
+        res.json(result);
+      }
+    });
+});
 
+
+
+// updateEmployeeList
+app.put('updateEmployeeList',(req,res)=>{
+  connection.query("UPDATE employeedetail SET first_name = ?, last_name = ?, email = ?, phone_number = ?, hire_date = ?, job_title = ?, department = ?, salary = ?, manager_id = ? WHERE employee_id = ?"
+  ,[req.body.first_name,req.body.last_name,req.body.email,req.body.phone_number,req.body.hire_date,req.body.job_title,req.body.department,req.body.salary,req.body.manager_id,req.body.employee_id],(error,result)=>{
+    if(error){
+      console.log('error is',error);
+      return error
+    }else{
+      console.log('The solution is',s);
+      res.json(result)
+    }
+  })
+})
+
+// deleteEmployeeList
+app.put('deleteEmployeeList',(req,res)=>{
+  connection.query("update employeedetails set isActive=0 where employee_id=?"[req.body.employee_id],(error,result)=>{
+    if(error){
+      console.log('error is',error);
+      return error
+    }else{
+      console.log('The solution is',s);
+      res.json(result)
+    }
+  })
+})
 
 app.listen(port,()=>{
     console.log('server is running',port)
